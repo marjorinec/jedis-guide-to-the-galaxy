f63268351d08e94f5aa9cd6947d9d7f7bf3fe44a
Alline Marjorie <allinemarjorie@gmail.com>
2019-07-17 21:42:52 -0300

    Joga a lógica de aleatorização de planetas pro componente principal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import Planet from './Planet';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  
+  async componentDidMount() {
+    await this.getPlanetCount()
+    this.randomPlanets()
+  }
+  
+  async getPlanetCount() {
+    const planetsData = await axios.get("https://swapi.co/api/planets/")
+    this.setState({planetCount: planetsData.data.count})
+  }
+
+  randomPlanets() {
+    let planetId = Math.ceil(Math.random() * this.state.planetCount)
+    this.setState({planetId: planetId})
+	}	
+
   render() {
-    return <Planet id="12"/>
+    return <Planet id={this.state.planetId}/>
   }
 }
 
--- src/Planet.js
+++ src/Planet.js
@@ -4,61 +4,75 @@ import axios from 'axios';
 class Planet extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {}
+		this.state = {
+			planetInfo: null,
+			currentPlanetId: null
+		}
 	}
 
-	async componentWillMount() {
-		const planet = await axios.get("https://swapi.co/api/planets/" + this.props.id)
+	async componentDidUpdate(prevProps, prevState) {
 		
-		const planetInfo = {
-			name: planet.data.name,
-			rotation_period: planet.data.rotation_period,
-			orbital_period: planet.data.orbital_period,
-			diameter: planet.data.diameter,
-			climate: planet.data.climate,
-			gravity: planet.data.gravity,
-			terrain: planet.data.terrain,
-			surface_water: planet.data.surface_water,
-			population: planet.data.population
+		if (this.props.id !== undefined  && this.props.id !== this.state.currentPlanetId){
+			const planet = await axios.get("https://swapi.co/api/planets/" + this.props.id)
+			const planetInfo = {
+				name: planet.data.name,
+				rotation_period: planet.data.rotation_period,
+				orbital_period: planet.data.orbital_period,
+				diameter: planet.data.diameter,
+				climate: planet.data.climate,
+				gravity: planet.data.gravity,
+				terrain: planet.data.terrain,
+				surface_water: planet.data.surface_water,
+				population: planet.data.population
+			}
+			this.setState({ planetInfo: planetInfo, currentPlanetId: this.props.id })
 		}
-		this.setState(planetInfo)
 	}
 
 	render() {
-		return (
-			<section className="planet-info">
-			<div className="planet-name">
-				{this.state.name}
+		console.log(this.state)
+		if (this.state.planetInfo === null) {
+			return (
+			<div>
+				carregando rs
 			</div>
-			<div className="planet-rotation_period">
-				{this.state.rotation_period}
-			</div>
-			<div className="planet-orbital_period">
-				{this.state.orbital_period}
-			</div>
-			<div className="planet-diameter">
-				{this.state.diameter}
-			</div>
-			<div className="planet-climate">
-				{this.state.climate}
-			</div>
-			<div className="planet-gravity">
-				{this.state.gravity}
-			</div>
-			<div className="planet-terrain">
-				{this.state.terrain}
-			</div>
-			<div className="planet-surface_water">
-				{this.state.surface_water}
-			</div>
-			<div className="planet-population">
-				{this.state.population}
-			</div>
-			<div className="random-planet-id">
-				{this.props.id}
-			</div>
-		</section>
 		)
+		} else {
+			return (
+				<section className="planet-info">
+					<div className="planet-name">
+						{this.state.planetInfo.name}
+					</div>
+					<div className="planet-rotation_period">
+						{this.state.planetInfo.rotation_period}
+					</div>
+					<div className="planet-orbital_period">
+						{this.state.planetInfo.orbital_period}
+					</div>
+					<div className="planet-diameter">
+						{this.state.planetInfo.diameter}
+					</div>
+					<div className="planet-climate">
+						{this.state.planetInfo.climate}
+					</div>
+					<div className="planet-gravity">
+						{this.state.planetInfo.gravity}
+					</div>
+					<div className="planet-terrain">
+						{this.state.planetInfo.terrain}
+					</div>
+					<div className="planet-surface_water">
+						{this.state.planetInfo.surface_water}
+					</div>
+					<div className="planet-population">
+						{this.state.planetInfo.population}
+					</div>
+					<div className="random-planet-id">
+						{this.props.id}
+					</div>
+				</section>
+			)
+		}
 	}  
 }
 
